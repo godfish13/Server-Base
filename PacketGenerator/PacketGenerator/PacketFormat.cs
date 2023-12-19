44bcd06d2fb1b187bf49c502eb13798e934d7185
godfish13 <63995325+godfish13@users.noreply.github.com>
2023-12-19 18:22:05 +0900

    Revert "20231214_packetgenerator#3"
    This reverts commit 96acc64eb83b16da2920dd608134478f01394f98.

--- PacketGenerator/PacketGenerator/PacketFormat.cs
+++ PacketGenerator/PacketGenerator/PacketFormat.cs
@@ -8,38 +8,12 @@ namespace PacketGenerator
 {
     internal class PacketFormat     // @""의 형태로 @를 붙여서 쓰면 아래처럼 쓸 수 있음
     {
-        // {0} : 패킷 이름/번호 목록
-        // {1} : 패킷 목록
-        public static string fileFormat =
-@"using ServerCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-
-public enum PacketIDEnum
-{{
-    {0}
-}}
-{1}";
-        // {0} : 패킷 이름
-        // {1} : 패킷 번호
-        public static string packetEnumFormat =
-@"{0} = {1},";
-
-
-
         // {0} 패킷 이름
         // {1} 멤버 변수들
         // {2} 멤버 변수 Read
         // {3} 멤버 변수 Write       // {}을 매개변수 말고 중괄호로 쓰고싶으면 {{ 이런식으로 2개 연달아 쓰면됨
         public static string packetFormat =
-@"
-
-class {0}
+@"class {0}
 {{
     {1}
 
@@ -87,9 +61,7 @@ class {0}
         // {3} 멤버 변수 Read
         // {4} 멤버 변수 Write 
         public static string memberListFormat =
-@"
-public List<{0}> {1}s = new List<{0}>();
-public class {0}
+@"public struct {0}
 {{
     {2}
 
@@ -106,30 +78,21 @@ public class {0}
         return success;
     }}
 }}
-";
+public List<{0}> {1}s = new List<{0}>();";
 
 
         // {0} 변수 이름
         // {1} To~~ 변수 형식
         // {2} 변수 형식
         public static string readFormat =
-@"
-// {2} {0} 읽기
+@"// 단순 변수 읽기
 this.{0} = BitConverter.{1}(s.Slice(count, s.Length - count));
 count += sizeof({2});";
 
         // {0} 변수 이름
-        // {1} 변수 형식
-        public static string readByteFormat =
-@"
-// {1} {0} 읽기
-this.{0} = ({1})segment.Array[segment.Offset + count];
-count += sizeof({1});";
-
-        // {0} 변수 이름
         public static string readStringFormat =
 @"
-// string {0} 읽기
+// string 읽기
 ushort {0}Length = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
 count += sizeof(ushort);
 this.{0} = Encoding.Unicode.GetString(s.Slice(count, nameLength));
@@ -139,7 +102,7 @@ count += {0}Length;";
         // {1} 리스트 이름 [소문자시작] : 멤버 변수로 사용할 리스트 이름
         public static string readListFormat =
 @"
-// {0} list {1}s 읽기 처리
+// list 처리 읽기
 {1}s.Clear(); // 시작전 한번 청소
 ushort {1}Length = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
 count += sizeof(ushort);
@@ -154,23 +117,14 @@ for(int i = 0; i < {1}Length; i++)
         // {0} 변수 이름
         // {1} 변수 형식
         public static string writeFormat =
-@"
-// {1} {0} 보내기
+@"// 단순 변수 보내기
 success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.{0});
-count += sizeof({1});";
-
-        // {0} 변수 이름
-        // {1} 변수 형식
-        public static string writeByteFormat =
-@"
-// {1} {0} 읽기
-openSegment.Array[openSegment.Offset + count] = ({1})this.{0};
 count += sizeof({1});";
 
         // {0} 변수 이름
         public static string writeStringFormat =
 @"
-// string {0} 보내기
+// string 보내기
 ushort {0}Length = (ushort)Encoding.Unicode.GetBytes(this.{0}, 0, this.{0}.Length, openSegment.Array, openSegment.Offset + count + sizeof(ushort));
 success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), {0}Length);
 count += sizeof(ushort);
@@ -180,7 +134,7 @@ count += {0}Length;";
         // {1} 리스트 이름 [소문자시작] : 멤버 변수로 사용할 리스트 이름
         public static string writeListFormat =
 @"
-// {0} list {1}s 보내기 처리
+// list 보내기
 success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)this.{1}s.Count);  // 스킬 갯수 직렬화, ushort로 메모리 아끼는거 기억!
 count += sizeof(ushort);
 foreach ({0} {1} in this.{1}s) // foreach를 통해 각 skill 마다 Write로 s에 직렬화해줌
